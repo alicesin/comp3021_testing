c1af059d3eadf8a85e945448b55b2e88eca2e500
Alice Sin <sinwinglamalice@gmail.com>
2017-08-12 23:05:12 +0800

    updated main msg

--- src/comp3021_testing/HelloWorld.java
+++ src/comp3021_testing/HelloWorld.java
@@ -1,7 +1,7 @@
 package comp3021_testing;
 
 public class HelloWorld {
-	public void main () {
-		System.out.println("Hello World!");
+	public static void main (String[] args) {
+		System.out.println("Hello World! updated!");
 	}
 }
